35a8ca458f0adce611d69ea59066187bb66ebadd
mehdi.houcine <mehdi.houcine@talan.com>
2020-07-07 13:17:48 +0200

    implement compute

--- something/src/main/java/training/lab/Service.java
+++ something/src/main/java/training/lab/Service.java
@@ -5,7 +5,14 @@ import java.math.BigDecimal;
 public class Service {
     public BigDecimal compute(final Household household) {
         // Expected behavior :
-        // - 1 adult with 32000 revenues should pay 3893.26 
+        // - 1 adult with 32000 revenues should pay 3893.26
+        if (null != household.getIndividuals().get(0)) {
+            BigDecimal totalIncome = household.getIndividuals().get(0).getRevenue();
+            Integer familuQuotient = null;
+            if (household.getIndividuals().get(0).getType() == IndividualType.ADULT) {
+                familuQuotient = 1;
+            }
+        }
         // - 2 adults with 32000 + 23950 revenues and 2
         return null;
     }
