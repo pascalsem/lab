433c62f35dc8bfc1ec891755897fa36a0db3af0e
Huawei <Huawei@192.168.1.3>
2022-09-24 00:28:30 +0200

    [feature]: Implement CiceService

--- otherthing/src/main/java/training/CiceService.java
+++ otherthing/src/main/java/training/CiceService.java
@@ -4,7 +4,27 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class CiceService {
-    public BigDecimal compute(final List<Employee> employees) {
-        return null;
-    }
+	 private static final BigDecimal MIN_WAGE = new BigDecimal("1014.00");
+	 BigDecimal MAX_INCOME = new BigDecimal("2.5").multiply(MIN_WAGE);
+	    public BigDecimal compute(final List<Employee> employees) {
+	        BigDecimal basis = new BigDecimal(0);
+	        for (final Employee employee : employees) {
+	            if (employee.getIntern().equals(false)) {
+	            	BigDecimal incomes = new BigDecimal(0);
+	                for (final Earning earning : employee.getEarnings()) {
+	                    if ((earning.getType() == EarningType.SALARY ||
+	                            earning.getType() == EarningType.OVERTIME ||
+	                            earning.getType() == EarningType.BONUS ||
+	                            earning.getType() == EarningType.VACATION_PAY)) {
+	                    	incomes = incomes.add(earning.getAmount());
+	                    }
+	                }
+	                if (incomes.compareTo(incomeSuperior) < 0) {
+	                    basis = basis.add(incomes);
+	                }
+	            }
+	        }
+	        BigDecimal cice = basis.multiply(new BigDecimal("0.06"));
+	        return cice.stripTrailingZeros();
+	    }
 }
